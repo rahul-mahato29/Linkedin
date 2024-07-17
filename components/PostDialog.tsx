06f69976035e0b2558077fc4e910c2b884d12f3b
Rahul Mahato <mahatorahul2901@gmail.com>
2024-07-17 08:52:01 +0530

    postDialog completed

--- components/PostDialog.tsx
+++ components/PostDialog.tsx
@@ -11,21 +11,28 @@ import {
 import { Input } from "@/components/ui/input";
 import ProfileImage from "./shared/ProfileImage";
 import { Textarea } from "./ui/textarea";
+import { Images } from "lucide-react";
+import { useRef, useState } from "react";
+import { readFileAsDataUrl } from "@/lib/utils";
+import Image from "next/image";
 
-export function PostDialog({
-  setOpen,
-  open,
-  src,
-}: {
-  setOpen: any;
-  open: any;
-  src: string;
-}) {
+export function PostDialog({ setOpen, open, src }: { setOpen: any; open: any; src: string; }) {
+  const inputRef = useRef<HTMLInputElement>(null);
+  const [selectedFile, setSelectedFile] =  useState<any>("");
+
+  const fileChangeHandler = async (e:React.ChangeEvent<HTMLInputElement>) => {
+    const file = e.target.files?.[0];
+    if(file){
+      const dataUrl = await readFileAsDataUrl(file);
+      setSelectedFile(dataUrl);
+    }
+  }
+
   return (
     <Dialog open={open}>
       <DialogContent
         onInteractOutside={() => setOpen(false)}
-        // className="sm:max-w-[425px]"
+        className="sm:max-w-[470px]"
       >
         <DialogTitle className="flex items-center gap-3">
             <ProfileImage url={src} />
@@ -34,12 +41,23 @@ export function PostDialog({
               <p className="text-sm font-normal">Post to Anyone </p>
             </div>
         </DialogTitle>
-        <form className="border-b border-gray-400 pb-2">
-            <Textarea id="name" name="inputText" className="border-none h-44 resize-none" placeholder="What do you want to talk about ?" />
+        <form className="border-b border-gray-400">
+            <Textarea id="name" name="inputText" className="border-none h-40 resize-none" placeholder="What do you want to talk about ?" />
+            <div className="my-4">
+              {
+                selectedFile && (
+                  <center><Image src={selectedFile} alt="preview-image" className="" width={200} height={200}/></center>
+                )
+              }
+            </div> 
         </form>
-        <DialogFooter className="">
-          <Button type="submit">Post</Button>
+        <DialogFooter>
+            <input ref={inputRef} onChange={fileChangeHandler} type="file" className="hidden" name="image" accept="image/*" />
         </DialogFooter>
+            <div className="flex items-center justify-between">
+              <Button onClick={() => inputRef?.current?.click()} variant={'link'} className="p-0 m-0"><Images className="text-blue-500"/></Button>
+              <Button type="submit">Post</Button>
+            </div>
       </DialogContent>
     </Dialog>
   );
--- lib/utils.ts
+++ lib/utils.ts
@@ -4,3 +4,15 @@ import { twMerge } from "tailwind-merge"
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+
+//converting the file into a url
+export const readFileAsDataUrl = (file: File | Blob):Promise<string> => {
+  return new Promise((resolve) => {
+    const fileReader = new FileReader();
+    fileReader.onloadend = () => {
+      if (typeof fileReader.result === 'string') return resolve(fileReader.result);
+    }
+    fileReader.readAsDataURL(file);
+  })
+}
